6588dc4c603e9472a3b054fcad08e3792eafe999
Julius <s3769579@mail.zih.tu-dresden.de>
2014-12-01 15:29:06 +0100

    bla

---
+++ classes/class.assReviewableMultipleChoiceQuestionGUI.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "./Modules/TestQuestionPool/classes/class.assQuestionGUI.php";
+include_once "./Modules/Test/classes/inc.AssessmentConstants.php";
+
+/**
+ * Example GUI class for question type plugins
+ *
+ * @author	Julius Felchow <julius.felchow@mailbox.tu-dresden.de>
+ * @version	$Id:  $
+ *
+ * @ingroup ModulesTestQuestionPool
+ *
+ * @ilctrl_iscalledby assExampleQuestionGUI: ilObjQuestionPoolGUI, ilObjTestGUI, ilQuestionEditGUI, ilTestExpressPageObjectGUI
+ */
+
+
+
+
+?>
