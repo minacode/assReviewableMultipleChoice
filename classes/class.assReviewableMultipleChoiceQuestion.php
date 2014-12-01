55786c1d4638690a8538dae942ad1a50daddbbb8
minacode <minamoto9@web.de>
2014-12-01 15:44:03 +0100

    grundgerüst

---
+++ classes/class.assReviewableMultipleChoiceQuestion.php
@@ -0,0 +1,68 @@
+<?php
+/* Copyright (c) 1998-2013 ILIAS open source, Extended GPL, see docs/LICENSE */
+
+/**
+ * Class for multiple choice tests.
+ *
+ * assReviewableMultipleChoice is a class that implements reviewable multiple choice questions.
+ *
+ * @extends assMultipleChoiceQuestion
+ * 
+ * @author		Julius Felchow <julius.felchow@mailbox.tu-dresden.de>
+ * @author		Max Friedrich <max.friedrich@mailbox.tu-dresden.de>
+ * 
+ * @ingroup		ModulesTestQuestionPool
+ */
+ 
+ require_once('./Modules/TestQuestionPool/classes/class.assMultipleChoice.php';);
+
+class assReviewableMultipleChoice extends assMultipleChoice {
+
+	protected $taxonomy;
+	protected $knowledge_dimension;
+
+	function _construct(
+		$title = "", 
+		$comment = "", 
+		$author = "", 
+		$owner = -1, 
+		$question = "", 
+		$output_type = OUTPUT_ORDER
+		$taxonomy = "",
+		$knowledge_dimension = ""
+	) {
+		parent::_construct($title, $comment, $author, $owner, $question, $output_type);
+		$this->taxonomy = $taxonomy;
+		$this->knowledge_dimension = $knowledge_dimension;
+	}
+	
+	function saveToDb($original_id = "") {
+		parent::_saveToDb($original_id);
+	}
+	
+	function loadFromDb($question_id) {
+		parent::_loadFromDb($question_id);
+	}
+	
+	function duplicate ($for_test=true, $title="", $author="", $owner="", $testObjId=null) {
+		parent::_duplicate($for_test, $title, $author, $owner, $testObjId);
+	}
+	
+	function copyObject ($target_questionpool_id, $title="") {
+		parent::copyObject($target_questionpool_id, $title);
+	}
+	
+	function createNewOriginalFromThisDuplicate($targetParentId, $targetQuestionTitle="") {
+		//...
+	}
+	
+	function toJSON() {
+		$result = json_decode( parent::toJson() );
+		
+		$result['taxonomy'] = $this->getTaxonomy();
+		$result['knowlegde_dimension'] = $this->getKnowlegdgeDimension();
+		
+		return json_encode($result);
+	}
+
+}
