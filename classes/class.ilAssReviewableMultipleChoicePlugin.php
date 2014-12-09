339f9dfbf99e7bc17facad0c21479ec538f55e2d
Julius <s3769579@mail.zih.tu-dresden.de>
2014-12-09 14:09:56 +0100

    strange bugfix°.°

---
+++ classes/class.ilAssReviewableMultipleChoicePlugin.php
@@ -0,0 +1,29 @@
+<?php
+require_once("./Modules/TestQuestionPool/classes/class.ilQuestionsPlugin.php");
+	
+/**
+* ReviewableMultipleChoice Plugin
+*
+* @author Julius Felchow <julius.felchow@mailbox.tu-dresden.de>
+* @author Max Friedrich <Max.Friedrich@mailbox.tu-dresden.de>
+* @version $Id$
+* 
+*/
+class ilassReviewableMultipleChoicePlugin extends ilQuestionsPlugin
+{
+		final function getPluginName()
+		{
+			return "assReviewableMultipleChoice";
+		}
+		
+		final function getQuestionType()
+		{
+			return "assReviewableMultipleChoice";
+		}
+		
+		final function getQuestionTypeTranslation()
+		{
+			return $this->txt($this->getQuestionType());
+		}
+}
+?>
